a297a678002fe343d69f0c4e01a3e31fce811a19
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-05-14 19:40:33 +0900

    create Bridging-Header

--- SweatOut/SweatOut/ViewController.swift
+++ SweatOut/SweatOut/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //BLEDeviceClassDelegate
 
+    static let uuidVspService = "569a1101-b87f-490c-92cb-11ba5ea5167c"
+    static let uuidRX = "569a2001-b87f-490c-92cb-11ba5ea5167c"
+    static let uuidTX = "569a2000-b87f-490c-92cb-11ba5ea5167c"
+    
+    var blBase = BLEBaseClass()
+    var device: BLEDeviceClass!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        blBase.scanDevices(nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func connect() {
+        
+    }
+    
+    func disconnect() {
+        
+    }
+    
+    //------------------------------------------------------------------------------------------
+    // BLEDeviceClassDelegate
+    // readもしくはindicateもしくはnotifyにてキャラクタリスティックの値を読み込んだ時に呼ばれる
+    //------------------------------------------------------------------------------------------
+    func didUpdateValueForCharacteristic(device: BLEDeviceClass!, characteristic: CBCharacteristic!) {
+        return
+    }
+    
 }
 
